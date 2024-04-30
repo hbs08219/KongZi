a390f59236f2d4ee02a33f62743b3bfe8e2d2499
hbs08219 <534108854@qq.com>
2024-04-30 20:44:26 +0800

    创建仓库

---
+++ Plugins/LogicDriver/Source/SMSystemEditor/Private/Utilities/SMEditorAuthentication.h
@@ -0,0 +1,61 @@
+﻿// Copyright Recursoft LLC 2019-2023. All Rights Reserved.
+
+#pragma once
+
+#include "Configuration/SMEditorSettings.h"
+#include "SMBlueprintEditorUtils.h"
+
+#include "Misc/SMAuthenticator.h"
+
+namespace LD::EditorAuth
+{
+	enum LDAuthStatus
+	{
+		Never = 0,
+		Succeeded = 1,
+		Failed = 2,
+	};
+
+	/** Retrieve the last recorded authentication state. */
+	inline LDAuthStatus GetUserMarketplaceAuthenticationStatus()
+	{
+		return static_cast<LDAuthStatus>(FSMBlueprintEditorUtils::GetEditorSettings()->UserMarketplaceAuthenticationState);
+	}
+	
+	/** Perform an entitlement check and record the result to the user's editor settings. */
+	inline void AuthenticateAndRecordResult(const FSimpleDelegate& InOnAuthenticatedDelegate,
+		IPluginWardenModule::EUnauthorizedErrorHandling InUnauthorizedErrorHandling =
+		IPluginWardenModule::EUnauthorizedErrorHandling::ShowMessageOpenStore,
+		bool bForceAuthenticate = false)
+	{
+#if LOGICDRIVER_IS_MARKETPLACE_BUILD
+		FSMBlueprintEditorUtils::GetMutableEditorSettings()->UserMarketplaceAuthenticationState = LDAuthStatus::Failed;
+		FSMBlueprintEditorUtils::GetMutableEditorSettings()->SaveConfig();
+#endif
+		
+		FSMAuthenticator::Get().Authenticate(FSimpleDelegate::CreateLambda([InOnAuthenticatedDelegate]()
+		{
+#if LOGICDRIVER_IS_MARKETPLACE_BUILD
+			FSMBlueprintEditorUtils::GetMutableEditorSettings()->UserMarketplaceAuthenticationState = LDAuthStatus::Succeeded;
+			FSMBlueprintEditorUtils::GetMutableEditorSettings()->SaveConfig();
+#endif
+			InOnAuthenticatedDelegate.ExecuteIfBound();
+		}),
+		InUnauthorizedErrorHandling, bForceAuthenticate);
+	}
+
+	/** Perform an entitlement check and record the result to the user's editor settings. */
+	inline void AuthenticateAndRecordResultSilently()
+	{
+		AuthenticateAndRecordResult(FSimpleDelegate(), IPluginWardenModule::EUnauthorizedErrorHandling::Silent);
+	}
+
+	/** Perform an entitlement check and record the result to the user's editor settings, only if this hasn't been done before. */
+	inline void AuthenticateAndRecordResultOnceSilently()
+	{
+		if (GetUserMarketplaceAuthenticationStatus() == LD::EditorAuth::Never)
+		{
+			AuthenticateAndRecordResultSilently();
+		}
+	}
+}
---
+++ Source/KongZi/KongZi.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KongZi.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KongZi, "KongZi" );
